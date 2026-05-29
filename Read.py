f6bda2a7291e2f6e49a596345cf14baeb31bb7be
MissionMars456 <MissionMars456@gmail.com>
2026-05-29 10:26:47 -0400

    Add project files

---
+++ Read.py
@@ -0,0 +1,14 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+(train_images, train_labels), _ = tf.keras.datasets.mnist.load_data()
+train_images = train_images / 255
+
+def showImage(i):
+    plt.imshow(train_images[i], cmap='Grays')
+    plt.tick_params(which='both', bottom=False, left=False, labelbottom=False, labelleft=False)
+    plt.colorbar()
+    plt.title(f"Target: {train_labels[i]}")
+    plt.show()
+
+showImage(0)
